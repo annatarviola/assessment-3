3e93937ccceaa94c76c43b7efbdcdeb599a04eb9
Lily Springer <lilyspringer@hotmail.com>
2023-02-20 16:12:54 -0700

    made syntax a little cleaner

--- about.js
+++ about.js
@@ -1,19 +1,13 @@
 console.log("hello world");
 
-
-
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
 	alert('Form submitted successfully!');
 }
 
-
 let form = document.querySelector('#contact');
-
 form.addEventListener('submit', handleSubmit);
 
-
 let catPic = document.querySelector('#cat-pic');
-
 catPic.addEventListener('mouseover', () => alert(`You're as cute as she is <3`))
