09b127ec3647d80c1227e4466248cddcc0e816f9
Lily Springer <lilyspringer@hotmail.com>
2023-02-20 15:42:50 -0700

    working on profile

--- about.js
+++ about.js
@@ -5,10 +5,15 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Form submitted successfully!');
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+
+let catPic = document.querySelector('#cat-pic');
+
+catPic.addEventListener('mouseover', () => alert(`You're as cute as she is <3`))
---
+++ profile.js
@@ -0,0 +1,7 @@
+let colorBtn = document.querySelector(`#color`)
+let placeBtn = document.querySelector(`#place`)
+let ritualBtn = document.querySelector(`#ritual`)
+
+colorBtn.addEventListener('click', () => alert(`My favorite color is burgundy.`))
+placeBtn.addEventListener('click', () => alert(`My favorite place is the Oregon coast.`))
+ritualBtn.addEventListener('click', () => alert(`My favorite ritual is the Abramelin.`))
